5649194b74a06a00284795a79e22a886e5e04ff2
Abrar-rawr <mabrara29@gmail.com>
2025-09-29 14:02:59 +0800

    Update PRAK004-2510817310001-MUHAMMADABRAR.py

--- PRAK004-2510817310001-MUHAMMADABRAR.py
+++ PRAK004-2510817310001-MUHAMMADABRAR.py
@@ -1,9 +1,6 @@
-red = "\033[1;31m"
-reset = "\033[0m"
-
-print("##############################\n")
-print("#                             #\n")
-print(f"#         {red}Nama Anda{reset}           #\n")
-print(f"#            {red}NIM{reset}              #\n")
-print("#                             #\n")
-print("##############################\n")
+print("##############################")
+print("#                            #")
+print("#       Muhammad Abrar       #")
+print("#       2510817310001        #")
+print("#                            #")
+print("##############################")
