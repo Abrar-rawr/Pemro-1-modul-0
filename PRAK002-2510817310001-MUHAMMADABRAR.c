a6a9510084bf9d9d0fa8c199468c3444cb0c19a5
Abrar-rawr <mabrara29@gmail.com>
2025-09-29 13:44:36 +0800

    Update PRAK002-2510817310001-MUHAMMADABRAR.c

--- PRAK002-2510817310001-MUHAMMADABRAR.c
+++ PRAK002-2510817310001-MUHAMMADABRAR.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main()
 {
-    printf("Selamat Pagi, Nama Anda\n);
-    printf("Selamat Siang, Nama Anda\n);
-    printf("Selamat Malam, Nama Anda\n);
+    printf("Selamat Pagi, Nama Anda\n");
+    printf("Selamat Siang, Nama Anda\n");
+    printf("Selamat Malam, Nama Anda\n");
     return 0;
 }
